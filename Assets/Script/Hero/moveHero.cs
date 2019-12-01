fd0417ab5f52c6f4c5a7d40ede0458d0d14c7b07
Michael <micha_45@hotmail.fr>
2019-12-01 21:17:02 +0100

    Système de Deplacement + saut

--- Assets/Script/Hero/moveHero.cs
+++ Assets/Script/Hero/moveHero.cs
@@ -6,28 +6,61 @@ public class moveHero : MonoBehaviour
 {
 	private Vector2 movement; 
 	public Vector2 speed;
-	private bool canJump;
+	private Vector3 pos;
+	public float jumpVelocity = 5f;
+	public float jumpCountDown = 5f;
+	private float jumpCountDownTimer;
+	private  bool isJumping = false;
+	private  bool isFalling = false;
+	public float maxJump = 3f;
+	private float maxJumpTimer;
+	
 	
     // Start is called before the first frame update
     void Start()
     {
-        
+       Time.timeScale = 1;
+	   jumpCountDownTimer = jumpCountDown;
+	   maxJumpTimer = maxJump;
     }
 
     // Update is called once per frame
     void Update()
     {
+			
+			if ( isFalling ) {
+				jumpCountDownTimer -= Time.deltaTime;
+			
+			}
+			
+			if ( isJumping ) {
+				maxJumpTimer -= Time.deltaTime;
+			
+			}
+			
+			if ( jumpCountDownTimer < 0 ){
+					jumpCountDownTimer = jumpCountDown;
+					
+					isFalling = false;
+			}
+			
+			if ( GetComponent<Rigidbody2D> ().velocity.y < 0 || maxJumpTimer < 0 ){
+					maxJumpTimer = maxJump;
+					isFalling = true;
+					isJumping = false;
+				}
+				
+			
 			float inputY = Input.GetAxis("Vertical");
 			float inputX = Input.GetAxis("Horizontal");
+			float jump = 0f;
 			
-			float movx = inputX*speed.x;
-			
-			if ( inputY <0 ){
-				inputY = 0;
+			if (inputY > 0 && isFalling == false ){
+					jump = jumpVelocity;
+					isJumping = true;
 			}
-			float movy = inputY*speed.y;
 			
-			movement = new Vector2(movx,movy);
+			movement = new Vector2(inputX*speed.x,jump);
 			GetComponent<Rigidbody2D> ().velocity = movement;
     }
 }
