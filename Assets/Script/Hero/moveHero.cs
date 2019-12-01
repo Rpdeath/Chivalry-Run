1c183ed78c60fa528c85bca9ac2a826e64be248f
Michael <micha_45@hotmail.fr>
2019-12-01 20:02:04 +0100

    Initial commit

---
+++ Assets/Script/Hero/moveHero.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveHero : MonoBehaviour
+{
+	private Vector2 movement; 
+	public Vector2 speed;
+	private bool canJump;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+			float inputY = Input.GetAxis("Vertical");
+			float inputX = Input.GetAxis("Horizontal");
+			
+			float movx = inputX*speed.x;
+			
+			if ( inputY <0 ){
+				inputY = 0;
+			}
+			float movy = inputY*speed.y;
+			
+			movement = new Vector2(movx,movy);
+			GetComponent<Rigidbody2D> ().velocity = movement;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Chivalry-Run
+ 
