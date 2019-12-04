d89203bb9200a32f3b6106365e354d4e41ea0c39
Rpdeath <45818301+Rpdeath@users.noreply.github.com>
2019-12-04 13:58:28 +0100

    scrolling background

--- Assets/Script/BackGround/ScrollingBackGround.cs
+++ Assets/Script/BackGround/ScrollingBackGround.cs
@@ -6,15 +6,41 @@ public class ScrollingBackGround : MonoBehaviour
 {
 
     public float speed = 0;
+    public GameObject BackGround;
+
+    private Vector3 coinBasGauche;
+    private Vector3 coinBasDroit;
+    private Vector3 coinHautGauche;
+    private Vector3 coinHautDroit;
+    private float borderLeft;
+    private float borderRight;
+
     // Start is called before the first frame update
     void Start()
     {
+        coinBasGauche = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        coinBasDroit = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0));
+        coinHautGauche = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0));
+        coinHautDroit = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
+        borderRight = coinHautDroit.x;
+        borderLeft = coinHautGauche.x;
+
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(Time.time * speed, 0f);
+        BackGround.transform.position = new Vector3(BackGround.transform.position.x - (Time.deltaTime * speed),
+                                            BackGround.transform.position.y,
+                                            BackGround.transform.position.z);
+
+        if (BackGround.transform.position.x < borderLeft)
+        {
+            BackGround.transform.position = new Vector3(borderRight,
+                                            BackGround.transform.position.y,
+                                            BackGround.transform.position.z);
+        }
     }
 }
